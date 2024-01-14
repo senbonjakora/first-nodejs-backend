89fd9b0f46185d8568089f62d91ddd3d2ce36c95
SaneyBinFaruk <saneybinfaruk@SaneyBinFaruks-iMac.local>
2024-01-14 21:58:54 +0600

    Add: new variable to default.js file

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,8 @@ const jws = require("jsonwebtoken");
 const config = require("config");
 
 function auth(req, res, next) {
+  if (!config.get("requireAuth")) return next();
+
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No Token Provided!");
 
@@ -15,6 +17,3 @@ function auth(req, res, next) {
 }
 
 module.exports = auth;
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-//   .eyJpZCI6IjY1YTE4ZjRjMDU3NDkyZjA1ZTU5MWMyZSIsImlhdCI6MTcwNTA4Njc5Nn0
-//   .m0cW4dMcLFWa2JvNIq53Gf4zwe81CsaUaFdOJ7V_cxM;
